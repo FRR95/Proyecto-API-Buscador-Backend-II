ce14f785806d5222a04e21a4e4066f9576a6a133
Fran <franrocher95@gmail.com>
2024-03-09 17:27:35 +0100

    feat:adding getUserProfile controller and endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,3 +26,28 @@ export const GetUsers = async (req, res) => {
    }
 }
 
+export const getUserProfile=async(req,res)=>{
+    try {
+        const userId=req.tokenData.userId
+        const userProfile=await User
+        .findById({
+        _id:userId
+        })
+
+        res.status(201).json({
+         success:true,
+         message:"User profile retrieved succesfully",
+         data:userProfile
+        })
+        
+    } catch (error) {
+        res.status(500).json(
+            {
+               success: false,
+               message: "ERROR",
+               error: error.message
+            }
+         )
+    }
+}
+
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {  GetUsers } from "../controllers/user.controller.js";
+import {  GetUsers, getUserProfile } from "../controllers/user.controller.js";
 import { auth } from "../middlewares/auth.js";
 import { isSuperAdmin } from "../middlewares/isSuperAdmin.js";
 
@@ -7,6 +7,7 @@ import { isSuperAdmin } from "../middlewares/isSuperAdmin.js";
 const router=Router();
 
 router.get('/',auth,isSuperAdmin,GetUsers)
+router.get('/profile',auth,getUserProfile)
 
 
 export default router;
