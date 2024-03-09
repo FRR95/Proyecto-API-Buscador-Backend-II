3318f6fd2edecf1168f209ea81ef339ca4ede710
Fran <franrocher95@gmail.com>
2024-03-09 13:10:35 +0100

    feat.creating project

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from "express"
+
+import 'dotenv/config'
+import { dbConnection } from "./database/db.js"
+
+
+import router from "./routes/router.js"
+
+const app = express()
+
+app.use(express.json())
+
+
+const PORT = process.env.PORT || 4001
+
+app.get('/api/healthy', (req, res) => {
+    res.status(202).json({
+        success: true,
+        message: "Server is healthy"
+    })
+})
+
+//AUTH
+app.use('/api',router)
+
+dbConnection()
+    .then(() => {
+        console.log("Database connected")
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
