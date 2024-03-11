e66f4861d8ad2b8c5755d57fb6c85330d552cde1
FRR95 <franrocher95@gmail.com>
2024-03-11 10:23:43 +0100

    update:updating seeders

--- src/database/seeders/seeders.js
+++ src/database/seeders/seeders.js
@@ -22,6 +22,17 @@ const userSeeder = async () => {
 				email: "superadmin@superadmin.com",
 				password: bcrypt.hashSync('12345678', 10),
 			},
+			{
+				_id: new mongoose.Types.ObjectId("65eebde6af5c1f55427ff54f"),
+				email: "user@user.com",
+				password: bcrypt.hashSync('12345678', 10),
+				role:"user"
+			},
+			{
+				_id: new mongoose.Types.ObjectId("65eebde6af5c2f55427ff54f"),
+				email: "superadmin2@superadmin2.com",
+				password: bcrypt.hashSync('12345678', 10),
+			},
 		]);
 
 		console.log("Users created");
@@ -36,16 +47,26 @@ const postSeeder = async () => {
 
 		const post = await Post.create([
 			{
-
 				title: "title1",
 				description: "description1",
-				userId:"65eebde6af5c0f55427ff54e"
+				userId:new mongoose.Types.ObjectId("65eebde6af5c0f55427ff54e"),
 			},
 			{
 				title: "title2",
 				description: "description2",
-				userId:"65eebde6af5c0f55427ff54f"
+				userId:new mongoose.Types.ObjectId("65eebde6af5c0f55427ff54f"),
 
+			},
+			{
+				title: "title3",
+				description: "description3",
+				userId:new mongoose.Types.ObjectId("65eebde6af5c1f55427ff54f"),
+
+			},
+			{
+				title: "title4",
+				description: "description4",
+				userId:new mongoose.Types.ObjectId("65eebde6af5c2f55427ff54f"),
 			},
 		]);
 
